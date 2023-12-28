b466b83094fca4ad4d601b1be239d049b3ade993
Andriikryl <andreyk557@gmail.com>
2023-12-28 20:01:19 +0200

    add time logic

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Container } from "@/components/container/Container";
 import TsPage from "@/components/tsPage/TsPage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import clsx from "clsx";
 import { NumberBlock } from "@/components/numberBlock/NumberBlock";
@@ -9,11 +9,17 @@ import { data } from "@/data/Data";
 import VisuallyHidden from "@/components/visuallyhidden/VisuallyHidden";
 import Home from "@/components/icons/Home";
 
+const tabs = ["30", "60", "120"];
+
 export default function Play() {
+  const [time, setTime] = useState(0);
   const [gameStatus, setGameStatus] = useState(false);
   const [current, setCurrent] = useState(0);
   const [answered, setAnswered] = useState(0);
   const [missed, setMissed] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [isTimeSelected, setIsTimeSelected] = useState(false);
+
   const handelClickSkip = () => {
     setCurrent((prev) => prev + 1);
     setMissed((prev) => prev + 1);
@@ -24,7 +30,35 @@ export default function Play() {
   };
   const handelClickPlay = () => {
     setGameStatus((prev) => !prev);
+    setIsTimerRunning((prev) => !prev);
   };
+  const handleButtonTimeClick = (value: number) => {
+    setTime(value);
+    setIsTimeSelected(true);
+  };
+
+  const handelClickBack = () => {
+    setGameStatus((prev) => !prev);
+    setAnswered(0);
+    setMissed(0);
+    setIsTimerRunning((prev) => !prev);
+    setIsTimeSelected(false);
+  };
+
+  const minutesString = String(Math.floor(time / 60)).padStart(2, "0");
+  const secondsString = String(time % 60).padStart(2, "0");
+  useEffect(() => {
+    if (isTimerRunning) {
+      const interval = setInterval(() => {
+        setTime((s) => Math.max(s - 1, 0));
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [isTimerRunning]);
+
   return (
     <TsPage Color={"#a2d2ff"}>
       <section className={styles.play}>
@@ -32,6 +66,12 @@ export default function Play() {
           <div className={styles.play__box}>
             {gameStatus ? (
               <div className={styles.wrapper}>
+                <div className={styles.time__wrapper}>
+                  <p className={styles.time}>
+                    {" "}
+                    {minutesString}:{secondsString}
+                  </p>
+                </div>
                 <div className={styles.word__block}>
                   <p className={styles.word}>{data[current].title}</p>
                 </div>
@@ -53,15 +93,42 @@ export default function Play() {
                     answered
                   </button>
                 </div>
-                <button className={styles.btn__back} onClick={handelClickPlay}>
+                <button className={styles.btn__back} onClick={handelClickBack}>
                   <VisuallyHidden>back home</VisuallyHidden>
                   <Home />
                 </button>
               </div>
             ) : (
-              <button className={styles.paly__btn} onClick={handelClickPlay}>
-                play
-              </button>
+              <div className={styles.flex__group}>
+                <p className={styles.time__title}>Chose game time</p>
+                <div className={styles.btn__groupTime}>
+                  <button
+                    onClick={() => handleButtonTimeClick(30)}
+                    className={styles.btn__time}
+                  >
+                    30
+                  </button>
+                  <button
+                    onClick={() => handleButtonTimeClick(60)}
+                    className={styles.btn__time}
+                  >
+                    60
+                  </button>
+                  <button
+                    onClick={() => handleButtonTimeClick(120)}
+                    className={styles.btn__time}
+                  >
+                    120
+                  </button>
+                </div>
+                <button
+                  className={styles.paly__btn}
+                  onClick={handelClickPlay}
+                  disabled={!isTimeSelected}
+                >
+                  play
+                </button>
+              </div>
             )}
           </div>
         </Container>
---
+++ src/components/utils/timers.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export function useNow(interval: number, enabled: boolean): number | undefined {
+  const [now, setNow] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    if (!enabled) {
+      setNow(undefined);
+      return;
+    }
+
+    const int = setInterval(() => {
+      setNow(Date.now());
+    }, interval);
+
+    return () => {
+      clearInterval(int);
+    };
+  }, [interval, enabled]);
+
+  return now;
+}
+
+export function  useInterval(interval: number, enabled: boolean, cb: (n: number) => void): void {
+  useEffect(() => {
+    if (!enabled) {
+      return;
+    }
+
+    const int = setInterval(() => {
+      console.log("interval");
+      cb(Date.now());
+    }, interval);
+
+    return () => {
+      clearInterval(int);
+    };
+  }, [interval, enabled, cb]);
+}
