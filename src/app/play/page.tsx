c43d4270a8d6257b281d0c00e64f9d70ea94ca28
Andriikryl <andreyk557@gmail.com>
2023-12-28 16:41:49 +0200

    add play functionality to the palyPage

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -6,8 +6,11 @@ import styles from "./styles.module.css";
 import clsx from "clsx";
 import { NumberBlock } from "@/components/numberBlock/NumberBlock";
 import { data } from "@/data/Data";
+import VisuallyHidden from "@/components/visuallyhidden/VisuallyHidden";
+import Home from "@/components/icons/Home";
 
 export default function Play() {
+  const [gameStatus, setGameStatus] = useState(false);
   const [current, setCurrent] = useState(0);
   const [answered, setAnswered] = useState(0);
   const [missed, setMissed] = useState(0);
@@ -19,33 +22,47 @@ export default function Play() {
     setCurrent((prev) => prev + 1);
     setAnswered((prev) => prev + 1);
   };
+  const handelClickPlay = () => {
+    setGameStatus((prev) => !prev);
+  };
   return (
     <TsPage Color={"#a2d2ff"}>
       <section className={styles.play}>
         <Container>
           <div className={styles.play__box}>
-            <h1 className={styles.play_title}>Play page</h1>
-            <div className={styles.word__block}>
-              <p className={styles.word}>{data[current].title}</p>
-            </div>
-            <div className={styles.score__group}>
-              <NumberBlock value={answered} className={styles.answered} />
-              <NumberBlock value={missed} className={styles.missed} />
-            </div>
-            <div className={styles.btn__group}>
-              <button
-                className={clsx(styles.control__btn, styles.skip__btn)}
-                onClick={handelClickSkip}
-              >
-                missed
+            {gameStatus ? (
+              <div className={styles.wrapper}>
+                <div className={styles.word__block}>
+                  <p className={styles.word}>{data[current].title}</p>
+                </div>
+                <div className={styles.score__group}>
+                  <NumberBlock value={answered} className={styles.answered} />
+                  <NumberBlock value={missed} className={styles.missed} />
+                </div>
+                <div className={styles.btn__group}>
+                  <button
+                    className={clsx(styles.control__btn, styles.skip__btn)}
+                    onClick={handelClickSkip}
+                  >
+                    missed
+                  </button>
+                  <button
+                    className={clsx(styles.control__btn, styles.acept__btn)}
+                    onClick={handelClickUnser}
+                  >
+                    answered
+                  </button>
+                </div>
+                <button className={styles.btn__back} onClick={handelClickPlay}>
+                  <VisuallyHidden>back home</VisuallyHidden>
+                  <Home />
+                </button>
+              </div>
+            ) : (
+              <button className={styles.paly__btn} onClick={handelClickPlay}>
+                play
               </button>
-              <button
-                className={clsx(styles.control__btn, styles.acept__btn)}
-                onClick={handelClickUnser}
-              >
-                answered
-              </button>
-            </div>
+            )}
           </div>
         </Container>
       </section>
--- src/components/icons/Home.tsx
+++ src/components/icons/Home.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="26"
+      height="26"
       viewBox="0 0 16 16"
       fill="none"
     >
